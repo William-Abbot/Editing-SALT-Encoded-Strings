b5f6b69db7879b08b603078678852c24c791afa4
William-Abbot <59292592+William-Abbot@users.noreply.github.com>
2022-01-18 14:24:54 -0600

    decisions 2

--- AddWorkers.py
+++ AddWorkers.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 
 step = {'supply':'','minutes':'','seconds':'','type':'','item':''}
@@ -7,25 +8,25 @@ types = {'structure': 0, 'unit': 1, 'morph': 2, 'upgrade': 3}
 
 bo_string = input("enter SALT encoded string: ")
 
-bo_string.split('~')[1]
+bo_string_lst = bo_string.split('~')[1]
 
 #m1 = input("\n\noptions:\n  0: insert workers manually  1: paste build order with workers included")
 
-with_worker = input("\n\npaste build order with workers included: \n")
+print("\n\npaste build order with workers included (press ctrl+D when finished): ")
+
+with_worker = sys.stdin.read()
 
 with_worker.lstrip()
 
-with_worker.split('\n')
+with_worker_lst = with_worker.split('\n')
 
 workers = ("Probe", "SCV", "Drone")
 
-for line in with_worker:
+#TODO: round starting supply to 12
+for line in with_worker_lst:
+    #print(line)
     if any([w in line for w in workers]):
-        print("worker detected")
+        None
+    pattern = re.compile(r'(/s
 
 
-while counter > bo_string.length():
-    for i in range(5):
-        
-    counter += 6
-    break
