ae43da8308ba0533a99dc46024ace74f209fc227
William-Abbot <59292592+William-Abbot@users.noreply.github.com>
2022-01-18 09:26:20 -0600

    making decisions 1

--- AddWorkers.py
+++ AddWorkers.py
@@ -2,10 +2,14 @@ import re
 
 
 step = {'supply':'','minutes':'','seconds':'','type':'','item':''}
+types = {'structure': 0, 'unit': 1, 'morph': 2, 'upgrade': 3}
+#4 is added to the supply number and every character that is decoded has 1 subtracted from it
+
+
 
 bo_string = input("enter SALT encoded string: ")
 
-pattern = re.compile(r'(*)(~)')
+bo_string.split('~')[1]
 
 while counter > bo_string.length():
     for i in range(5):
